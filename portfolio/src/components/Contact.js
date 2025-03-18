f398dc82ddb9fc2730540a191de44533b27972ff
Lucas Furlan <155035762+furlanflucas@users.noreply.github.com>
2025-03-18 00:06:03 +0000

    copyright

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -6,8 +6,12 @@ const Contact = () => {
     <section className={styles.contact} id="contact">
       <p><a href="https://www.linkedin.com/in/furlanflucas/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
       <p><a href="https://github.com/furlanflucas" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+
+      {/* Copyright Notice */}
+      <p className={styles.copyright}>© 2025 Lucas Furlan. All Rights Reserved.</p>
     </section>
   );
 };
 
 export default Contact;
+
